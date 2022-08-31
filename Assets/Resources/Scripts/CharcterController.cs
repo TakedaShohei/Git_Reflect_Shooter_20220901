ac9c8aec59de7c17c740b0f254d1d700de80b1b5
Shohei Takeda <takeda.syouhei@gmail.com>
2022-08-31 22:48:39 +0900

    make character

--- Assets/Resources/Scripts/CharcterController.cs
+++ Assets/Resources/Scripts/CharcterController.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class CharcterController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    GameObject character_;
+    Rigidbody2D rigdbody_;
+    private float speed_ = 200;
+
     void Start()
     {
-        
+        Setup();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+        }
+
+    }
+
+    void Setup()
+    {
+        character_ = this.gameObject;
+        this.rigdbody_ = GetComponent<Rigidbody2D>();
     }
 }
