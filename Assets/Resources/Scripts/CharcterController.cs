499b88d43de5474a5c7f6538d3500d3668ae4186
Shohei Takeda <takeda.syouhei@gmail.com>
2022-09-02 20:49:30 +0900

    edit character controller
    add function of reflect

--- Assets/Resources/Scripts/CharcterController.cs
+++ Assets/Resources/Scripts/CharcterController.cs
@@ -1,80 +1,104 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets.Resources.Scripts { 
-public class CharcterController : MonoBehaviour
+namespace Assets.Resources.Scripts
 {
-    // Start is called before the first frame update
-
-
-    //parameter
-    int hp_;
-    int max_hp_;
-    float max_speed_;
-    private float speed_;
-
-    //set bounce
-    GameObject character_;
-    Rigidbody2D rigdbody_;
-    Vector2 start_pos_;
-
-    //CharacterData
-    [SerializeField]
-    CharacterData data_ = null;
-
-    [SerializeField]
-    Vector2 launch_direction = new Vector2();
-
-    [SerializeField]
-    SpriteRenderer render_ = null;
-
-    void Start()
+    public class CharcterController : MonoBehaviour
     {
-        Setup();
-    }
+        // Start is called before the first frame update
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
+
+        //parameter
+        int hp_;
+        int max_hp_;
+        float max_speed_;
+        private float speed_;
+
+        //set bounce
+        GameObject character_;
+        Rigidbody2D rigdbody_;
+        Vector2 start_pos_;
+
+        //CharacterData
+        [SerializeField]
+        CharacterData data_ = null;
+
+        [SerializeField]
+        Vector2 launch_direction = new Vector2();
+
+        [SerializeField]
+        SpriteRenderer render_ = null;
+
+        [SerializeField]
+        Transform root_ = null;
+
+        [SerializeField]
+        Vector2 current_direction = new Vector2();
+
+        void Start()
         {
-            speed_ = 100;
-            this.rigdbody_.AddForce(transform.up * speed_);
+            Setup();
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                speed_ = 100;
+                this.rigdbody_.AddForce(transform.up * speed_);
+            }
+
+            
+            current_direction = this.rigdbody_.velocity;
+        }
+
+        private void FixedUpdate()
+        {
+            this.rigdbody_.velocity *= 0.995f;
+        }
+
+        void Setup()
+        {
+            character_ = this.gameObject;
+            this.rigdbody_ = GetComponent<Rigidbody2D>();
+            speed_ = 0;
+            //setup character data
+            CharacterSetup(data_);
+
+        }
+
+
+
+        void test()
+        {
+            Launch();
+        }
+        void Launch()
+        {
+            rigdbody_.velocity = launch_direction;
+        }
+
+        void BounceUpdate()
+        {
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var disired_direction = Vector2.Reflect(current_direction,collision.contacts[0].normal);
+        }
+
+        public void CharacterSetup(CharacterData in_data)
+        {
+
+            hp_ = in_data.Hp;
+            max_hp_ = in_data.Hp;
+            max_speed_ = in_data.Speed;
+            speed_ = max_speed_;
+            render_.sprite = in_data.Image;
+
+            //just test
+            test();
+        }
     }
-
-    private void FixedUpdate()
-    {
-        this.rigdbody_.velocity *= 0.995f; 
-    }
-
-    void Setup()
-    {
-        //setup character data
-        CharacterSetup(data_);
-
-
-
-        character_ = this.gameObject;
-        this.rigdbody_ = GetComponent<Rigidbody2D>();
-        speed_ = 0;
-    }
-
-    void Launch()
-    {
-        rigdbody_.velocity = launch_direction;
-    }
-
-    public void CharacterSetup(CharacterData in_data)
-    {
-
-        hp_ = in_data.Hp;
-        max_hp_ = in_data.Hp;
-        max_speed_ = in_data.Speed;
-        speed_ = max_speed_;
-        render_.sprite = in_data.Image;
-    }
-}
 }
